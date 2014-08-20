dfffaed73ae683ac1b783f22eb4aa14016ee4e34
Cristián Romo <cristian.g.romo@gmail.com>
2014-08-20 01:45:34 -0500

    Second clean up pass of the main script.

--- model2dsgx.py
+++ model2dsgx.py
@@ -14,56 +14,77 @@ from fbx import *
 import sys
 import os
 
-if len(sys.argv) < 2:
-    print("Usage: model2dsgx.py [file to convert]")
-    sys.exit(1)
+def main(args):
+    if not valid_command_line_arguments(args):
+        print("Usage: %s [file to convert]" % args[0])
+        sys.exit(1)
 
-inputname = sys.argv[1]
+    input_filename = args[1]
+    output_filename = substitute_extension(input_filename, ".dsgx")
 
-# Get the extension and decide which importer to use from it.
-file_root, file_extension = os.path.splitext(inputname)
+    if not known_file_type(input_filename):
+        print("Unrecognized file type: " + file_extension(input_filename))
+        sys.exit(1)
+
+    model_to_convert = load_model_from_file(input_filename)
+
+    display_model_info(model_to_convert)
+
+    save_model_to_file(model_to_convert, output_filename)
+
+def valid_command_line_arguments(args):
+    return len(args) == 2
+
+def substitute_extension(filename, extension):
+    return os.path.splitext(filename)[0] + extension
+
+def known_file_type(filename):
+    return file_extension(filename) in _readers
+
+def file_extension(filename):
+    return os.path.splitext(filename)[1]
+
+def load_model_from_file(filename):
+    return _readers[file_extension(filename)](filename)
+
+def display_model_info(model):
+    print("Polygons: %d" % len(model.polygons))
+    print("Vertecies: %d" % len(model.vertecies))
+
+    # Count the number of texture maps in the model.
+    tex = 0
+    for poly in model.polygons:
+        if poly.uvlist != None:
+            tex += 1
+
+    print("Textured Polygons: %d" % tex)
+
+    print("Bounding Sphere: %s" % str(model.bounding_sphere()))
+    print("Bounding Box: %s" % str(model.bounding_box()))
+
+    print("Calculating culling cost...")
+    print("Worst-case Draw Cost (in polys): %d" % model.max_cull_polys())
+
+def save_model_to_file(model, filename):
+    print("Attempting output...")
+    dsgx.Writer().write(filename, model)
+    print("Output Successful!")
 
 def read_autodesk_fbx(filename):
     print("--Parsing FBX file--")
     #todo: not do all of this right here
-    model = fbx_importer.Reader().read(inputname)
+    model = fbx_importer.Reader().read(filename)
     print("Not fully implemented! PANIC!")
     return model
 
 def read_wavefront_obj(filename):
     print("---Parsing OBJ file---")
-    return obj_importer.Reader().read(inputname)
+    return obj_importer.Reader().read(filename)
 
-readers = {
+_readers = {
     ".fbx": read_autodesk_fbx,
     ".obj": read_wavefront_obj
 }
-if file_extension not in readers:
-    print("Unrecognized extension: " + file_extension)
-    sys.exit(1)
-model_to_convert = readers[file_extension](inputname)
 
-outfilename = file_root + '.dsgx'
-
-# Display information about the model.
-print("Polygons: %d" % len(model_to_convert.polygons))
-print("Vertecies: %d" % len(model_to_convert.vertecies))
-
-# Count the number of texture maps in the model.
-tex = 0
-for poly in model_to_convert.polygons:
-    if poly.uvlist != None:
-        tex += 1
-
-print("Textured Polygons: %d" % tex)
-
-print("Bounding Sphere: %s" % str(model_to_convert.bounding_sphere()))
-print("Bounding Box: %s" % str(model_to_convert.bounding_box()))
-
-print("Calculating culling cost...")
-print("Worst-case Draw Cost (in polys): %d" % model_to_convert.max_cull_polys())
-
-# Write the .dsgx file from the in memory model.
-print("Attempting output...")
-dsgx.Writer().write(outfilename, model_to_convert)
-print("Output Successful!")
+if __name__ == '__main__':
+    main(sys.argv)
