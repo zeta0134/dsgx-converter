7f0fc60152e5ab693ccd3937fbaa0858e9c4f6f2
Cristián Romo <cristian.g.romo@gmail.com>
2014-08-20 02:06:45 -0500

    Clean up of main script completed (for now).

--- model2dsgx.py
+++ model2dsgx.py
@@ -5,35 +5,31 @@ Created on Thu Oct 21 00:31:54 2010
 
 @author: Nicholas Flynt, Cristián Romo
 """
-from model import obj_importer
-from model import fbx_importer
-from model import dsgx
-
-from fbx import *
-
-import sys
-import os
+import os, sys
+from model import dsgx, fbx_importer, obj_importer
 
 def main(args):
     if not valid_command_line_arguments(args):
-        print("Usage: %s [file to convert]" % args[0])
-        sys.exit(1)
+        error_exit(1, "Usage: %s [file to convert]" % args[0])
 
     input_filename = args[1]
     output_filename = substitute_extension(input_filename, ".dsgx")
 
     if not known_file_type(input_filename):
-        print("Unrecognized file type: " + file_extension(input_filename))
-        sys.exit(1)
+        error_exit(1,
+            "Unrecognized file type: " + file_extension(input_filename))
 
-    model_to_convert = load_model_from_file(input_filename)
-
+    model_to_convert = load_model(input_filename)
     display_model_info(model_to_convert)
-
-    save_model_to_file(model_to_convert, output_filename)
+    save_model_as_dsgx(model_to_convert, output_filename)
 
 def valid_command_line_arguments(args):
     return len(args) == 2
+
+def error_exit(status_code, error_message=""):
+    if error_message:
+        print(error_message)
+    sys.exit(status_code)
 
 def substitute_extension(filename, extension):
     return os.path.splitext(filename)[0] + extension
@@ -44,35 +40,28 @@ def known_file_type(filename):
 def file_extension(filename):
     return os.path.splitext(filename)[1]
 
-def load_model_from_file(filename):
+def load_model(filename):
     return _readers[file_extension(filename)](filename)
 
 def display_model_info(model):
     print("Polygons: %d" % len(model.polygons))
     print("Vertecies: %d" % len(model.vertecies))
 
-    # Count the number of texture maps in the model.
-    tex = 0
-    for poly in model.polygons:
-        if poly.uvlist != None:
-            tex += 1
-
-    print("Textured Polygons: %d" % tex)
+    textured_polygons = sum(1 for polygon in model.polygons if polygon.uvlist)
+    print("Textured Polygons: %d" % textured_polygons)
 
     print("Bounding Sphere: %s" % str(model.bounding_sphere()))
     print("Bounding Box: %s" % str(model.bounding_box()))
 
-    print("Calculating culling cost...")
-    print("Worst-case Draw Cost (in polys): %d" % model.max_cull_polys())
+    print("Worst-case Draw Cost (polygons): %d" % model.max_cull_polys())
 
-def save_model_to_file(model, filename):
+def save_model_as_dsgx(model, filename):
     print("Attempting output...")
     dsgx.Writer().write(filename, model)
     print("Output Successful!")
 
 def read_autodesk_fbx(filename):
     print("--Parsing FBX file--")
-    #todo: not do all of this right here
     model = fbx_importer.Reader().read(filename)
     print("Not fully implemented! PANIC!")
     return model
